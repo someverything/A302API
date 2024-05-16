ffa274e7f8c8979a76781fc0f975f5a756f64d87
someverything <rustameyvaz@gmail.com>
2024-05-16 13:31:21 -0700

    Changes-2

---
+++ DataAccess/Concrete/EntityFramework/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost; Database = A302APIDB; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductLang> ProductLangs { get; set; }
+        public DbSet<CategoryLang> CategoryLangs { get; set; }
+        public DbSet<ProductPhoto> ProductPhotos { get; set; }
+
+    }
+}
